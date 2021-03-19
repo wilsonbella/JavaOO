987e77c49f35864bd91dd84d200cfe7f85066ce8
wilson bella <wilsonbella880@gmail.com>
2021-03-19 11:01:54 -0300

    Alteração de arquivo

--- src/bytebank/TesteReferencias.java
+++ src/bytebank/TesteReferencias.java
@@ -1,24 +1,26 @@
 package bytebank;
 
 public class TesteReferencias {
- public static void main(String[] args) {
-	Conta primeiraConta = new Conta();
-	primeiraConta.saldo = 300;
-	
-	System.out.println("saldo da primeira conta: " + primeiraConta.saldo );
-	
-	Conta segundaConta = primeiraConta;
-	
-	System.out.println("saldo da segunda conta: " + segundaConta.saldo);
-	
-	segundaConta.saldo += 100;
-	
-	System.out.println("saldo da segunda conta: " + segundaConta.saldo);
-	
-	System.out.println(primeiraConta.saldo);
-	
-	if(primeiraConta == segundaConta) {
-		System.out.println("São as mesmas contas");
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+
+		System.out.println("saldo da primeira conta: " + primeiraConta.saldo);
+
+		Conta segundaConta = primeiraConta;
+
+		System.out.println("saldo da segunda conta: " + segundaConta.saldo);
+
+		segundaConta.saldo += 100;
+
+		System.out.println("saldo da segunda conta: " + segundaConta.saldo);
+
+		System.out.println(primeiraConta.saldo);
+
+		if (primeiraConta == segundaConta) {
+			System.out.println("São as mesmas contas !!!");
+		} else {
+			System.out.println("Não são as mesmas contas !!!");
+		}
 	}
-}
 }
