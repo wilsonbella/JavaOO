7cc93d67f3cedf6bde4bdbf3ac8a2eb16a1e48a4
wilson bella <wilsonbella880@gmail.com>
2021-03-19 17:38:59 -0300

    Criado o método saca

--- src/bytebank/TestaMetodo.java
+++ src/bytebank/TestaMetodo.java
@@ -6,7 +6,9 @@ public class TestaMetodo {
 		contaDoWilson.saldo = 100;
 		contaDoWilson.deposita(50);
 		System.out.println(contaDoWilson.saldo);
-		
+		boolean conseguiuSacar = contaDoWilson.saca(20);
+		System.out.println(contaDoWilson.saldo);
+		System.out.println(conseguiuSacar);
 		
 	}
 }
