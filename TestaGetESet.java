d4673bbabfffd1adac1a419e838fda2c6cae63e3
wilson bella de oliveira <67123591+wilsonbella@users.noreply.github.com>
2021-03-29 20:09:27 -0300

    Concluído os getters e setters

---
+++ TestaGetESet.java
@@ -0,0 +1,31 @@
+package bytebankencapsulado;
+
+public class TestaGetESet {
+	public static void main(String[] args) {
+
+		Conta conta = new Conta();
+		// conta.numero = 1337;
+		conta.setNumero(1337);
+		System.out.println(conta.getNumero());
+
+		Cliente wilson = new Cliente();
+		// conta.titular = wilson;
+		wilson.setNome("Wilson Bella");
+
+		conta.setTitular(wilson);
+
+		System.out.println(conta.getTitular().getNome());
+
+		conta.getTitular().setProfissao("Programador");
+		// agora em duas linhas:
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(wilson);
+		System.out.println(conta.getTitular());
+		
+		
+	}
+
+}
