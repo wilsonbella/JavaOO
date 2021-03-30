a5782af1537e1c001e978ff27e7ad36d604bf930
wilson bella de oliveira <67123591+wilsonbella@users.noreply.github.com>
2021-03-30 17:30:44 -0300

    Criado a classe TestaValores e um construtor

--- Conta.java
+++ Conta.java
@@ -1,9 +1,18 @@
 package bytebankencapsulado;
+
 public class Conta {
 	private double saldo;
 	private int agencia;
 	private int numero;
 	private Cliente titular;
+	
+	
+	public Conta(int agencia, int numero) {
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("Estou criando uma conta " +this.numero);
+	}
+	
 
 	public void deposita(double valor) {
 		this.saldo += valor;
@@ -27,26 +36,41 @@ public class Conta {
 		}
 		return false;
 	}
-	
+
 	public double getSaldo() {
 		return this.saldo;
 	}
-	
+
 	public int getNumero() {
 		return this.numero;
 	}
-	public void setNumero(int numero){
+
+	public void setNumero(int numero) {
+		if(numero <= 0) {
+			System.out.println("Não pode número menor ou igual a 0");
+			return;
+		}
 		this.numero = numero;
 	}
+
 	public int getAgencia() {
+
 		return this.agencia;
 	}
+
 	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+
+			System.out.println("Não pode valor menor ou igual a 0");
+			return;
+		}
 		this.agencia = agencia;
 	}
+
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
 	}
+
 	public Cliente getTitular() {
 		return titular;
 	}
