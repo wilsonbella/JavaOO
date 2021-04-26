18e5aefffed471a179eea1518d532bda1096e76e
wilson bella de oliveira <67123591+wilsonbella@users.noreply.github.com>
2021-04-26 16:32:42 -0300

    Lançando exceções

--- java-pilha/src/FluxoComTratamento.java
+++ java-pilha/src/FluxoComTratamento.java
@@ -1,5 +1,5 @@
 
-public class Fluxo {
+public class FluxoComTratamento {
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
 		try {
@@ -22,8 +22,14 @@ public class Fluxo {
 
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
-		throw new ArithmeticException("Deu errado:(");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
 
-		// System.out.println("Fim do metodo2");
+			 int a = i / 0;
+			//Conta c = null;
+			//c.deposita();
+
+		}
+		System.out.println("Fim do metodo2");
 	}
 }
