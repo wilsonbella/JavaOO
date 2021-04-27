5726fc2129fd1f365e1fbc11eb4ce83775b3bd46
wilson bella de oliveira <67123591+wilsonbella@users.noreply.github.com>
2021-04-27 18:24:45 -0300

    Sacando com Checked Exception

--- bytebank-herdado-conta/src/TesteContas.java
+++ bytebank-herdado-conta/src/TesteContas.java
@@ -1,7 +1,7 @@
 
 public class TesteContas {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws SaldoInsuficienteException {
 
 		ContaCorrente cc = new ContaCorrente(121, 121);
 		cc.deposita(100.0);
--- bytebank-herdado-conta/src/TesteSaca.java
+++ bytebank-herdado-conta/src/TesteSaca.java
@@ -3,8 +3,15 @@ public class TesteSaca {
 
 		public static void main(String[] args) {
 			Conta conta = new ContaCorrente(123, 321);
+			
 			conta.deposita(200.0);
+			try {
 			conta.saca(200.0);
+			
+			}catch (SaldoInsuficienteException ex) {
+				
+				System.out.println("Ex: " + ex.getMessage());
+			}
 			
 			System.out.println(conta.getSaldo());
 		}
